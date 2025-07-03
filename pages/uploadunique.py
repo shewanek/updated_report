7f53a7e706c7389260c7bf4b0bc26ac54188f49b
shewanek <zshewanek@gmail.com>
2025-07-03 08:36:48 +0300

    update

--- pages/uploadunique.py
+++ pages/uploadunique.py
@@ -28,10 +28,10 @@ def main():
         #MainMenu { visibility: hidden; }
         .stDeployButton { visibility: hidden; }
         .stAppHeader { visibility: hidden; }
-        /* Hide the entire header bar */
-        header.stAppHeader {
-            display: none;
-        }
+        # /* Hide the entire header bar */
+        # header.stAppHeader {
+        #     display: none;
+        # }
         .stButton button {
             background-color: #000000;
             border: 1px solid #ccc;
